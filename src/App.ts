50b53245440a186f0491f273b7c755284c89858b
Michal Rehout <rehout@elephant-orchestra.com>
2018-06-09 22:49:57 +0200

    [MOD] Added Firebase app initialization to app constructor

--- src/App.ts
+++ src/App.ts
@@ -2,6 +2,8 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import UsersRouter from "./Routes/UsersRouter";
 import ContactsRouter from "./Routes/ContactsRouter";
+import {initializeApp as fbInitiaizeApp, credential as fbCredential} from "firebase-admin";
+import Settings from "./Settings";
 
 class App {
 
@@ -9,7 +11,11 @@ class App {
         this.app = express();
         this.app.use(bodyParser.json());
         this.app.use("/contacts", (req, res, next) => ContactsRouter(req, res, next));
-        this.app.use("/users", (req, res, next) => UsersRouter(req, res, next))
+        this.app.use("/users", (req, res, next) => UsersRouter(req, res, next));
+        fbInitiaizeApp({
+            credential: fbCredential.cert(Settings.fbServiceAccount),
+            databaseURL: Settings.fbUrl
+        });
     }
 
     app: express.Application;
--- src/Settings.ts
+++ src/Settings.ts
@@ -7,11 +7,13 @@ class Settings {
         this.passwordPepper = "pwdSecretPepper";
         this.userDatabaseFile = "memory";
         this.fbServiceAccount = require ("../keys/yo-yolo-firebase.json");
+        this.fbUrl = "https://yo-yolo.firebaseio.com";
     }
 
     jwtSecret: string;
     passwordPepper: string;
     userDatabaseFile: string;
     fbServiceAccount: FBServiceAccount;
+    fbUrl: string;
 }
 export default new Settings();
