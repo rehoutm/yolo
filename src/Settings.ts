ced097eea33ba281cc912730df6827965690dee4
Michal Rehout <rehout@elephant-orchestra.com>
2018-06-10 00:30:14 +0200

    [REF] Loading settings from ENV, using dotenv for dev purposes

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,13 +1,16 @@
 import { ServiceAccount as FBServiceAccount } from "firebase-admin";
+import { load as dotEnvLoad } from "dotenv";
 
 class Settings {
     constructor() {
-        //TODO load from env
-        this.jwtSecret = "jwtVerySecretKey";
-        this.passwordPepper = "pwdSecretPepper";
-        this.userDatabaseFile = "memory";
-        this.fbServiceAccount = require ("../keys/yo-yolo-firebase.json");
-        this.fbUrl = "https://yo-yolo.firebaseio.com";
+        if (process.env["NODE_ENV"] !== "production") {
+            dotEnvLoad();
+        }
+        this.jwtSecret = process.env["JWT_SECRET"];
+        this.passwordPepper = process.env["PWD_PEPPER"];
+        this.userDatabaseFile = process.env["DB_FILE"];
+        this.fbServiceAccount = require(process.env["FB_ACC_FILE"]);
+        this.fbUrl = process.env["FB_URL"];
     }
 
     jwtSecret: string;
